429ef2b99f95e235324d4836a13bc1fe917215ba
P115189 <winjit.samadhan.medge@fullertonindia.com>
2021-07-19 14:55:41 +0530

    Schedule Video call UI Design and Implementation

---
+++ lib/src/ui/schedule_video_call_screen.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+import 'package:gin_finans_app/src/listeners/sub_screen_callback_listener.dart';
+
+class ScheduleVideoCallScreen extends StatefulWidget {
+  final SubScreenCallbackListener subScreenCallbackListener;
+
+  ScheduleVideoCallScreen({required this.subScreenCallbackListener});
+  @override
+  _ScheduleVideoCallScreenState createState() => _ScheduleVideoCallScreenState();
+}
+
+class _ScheduleVideoCallScreenState extends State<ScheduleVideoCallScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
