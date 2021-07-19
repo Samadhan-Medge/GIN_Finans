dab7982dd197424b5a038deadff8f353c32c1bb9
P115189 <winjit.samadhan.medge@fullertonindia.com>
2021-07-19 15:26:52 +0530

    Added Next Button for Personal Information screen

--- lib/src/ui/personal_information_screen.dart
+++ lib/src/ui/personal_information_screen.dart
@@ -1,11 +1,14 @@
-import 'package:flutter/material.dart';
 import 'dart:convert';
+
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:gin_finans_app/src/listeners/sub_screen_callback_listener.dart';
 import 'package:gin_finans_app/src/repositories/data_model.dart';
 import 'package:gin_finans_app/src/repositories/monthly_expense_model.dart';
 import 'package:gin_finans_app/src/ui/custom_ui/custom_dropdown.dart';
 import 'package:gin_finans_app/src/values/dimensions.dart';
+
+import 'custom_ui/next_button.dart';
 
 class PersonalInformationScreen extends StatefulWidget {
   final SubScreenCallbackListener subScreenCallbackListener;
@@ -22,71 +25,96 @@ class _PersonalInformationScreenState extends State<PersonalInformationScreen> {
   String? _selectedMonthlyExpense;
 
   DataModel? _model;
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: FutureBuilder<DataModel>(
-          future: getDropDownDataModel(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Scrollbar(
-                child: Column(
-                  children: [
-                    Text("Personal Information", style: Theme.of(context).textTheme.bodyText1),
-                    SizedBox(height: 10.0),
-                    Text("Please fill in the information below and your goal for digital saving",
-                        style: Theme.of(context).textTheme.bodyText1!.copyWith(fontSize: AppDimensions.textSizeSmall)),
-                    CustomDropDown(
-                      hint: 'Goal for activation',
-                      dropdownMenuItems: buildDropDownMenuItems(snapshot.data!.goalOfActivation),
-                      selectedItem: _selectedGoalItem!,
-                      callBack: (selectedValue) {
-                        setState(() {
-                          _selectedGoalItem = selectedValue;
-                        });
-                      },
+    return FutureBuilder<DataModel>(
+      future: getDropDownDataModel(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: MediaQuery.removePadding(
+                    context: context,
+                    removeTop: true,
+                    child: ListView(
+                      shrinkWrap: false,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Personal Information", style: Theme.of(context).textTheme.bodyText1),
+                            SizedBox(height: 10.0),
+                            Text("Please fill in the information below and your goal for digital saving",
+                                style: Theme.of(context).textTheme.bodyText1!.copyWith(fontSize: AppDimensions.textSizeSmall)),
+                            SizedBox(height: 20.0),
+                            CustomDropDown(
+                              hint: 'Goal for activation',
+                              dropdownMenuItems: buildDropDownMenuItems(snapshot.data!.goalOfActivation),
+                              selectedItem: _selectedGoalItem ?? null,
+                              callBack: (selectedValue) {
+                                setState(() {
+                                  _selectedGoalItem = selectedValue;
+                                });
+                              },
+                            ),
+                            SizedBox(
+                              height: 30,
+                            ),
+                            CustomDropDown(
+                              hint: 'Monthly Income',
+                              dropdownMenuItems: buildDropDownMenuItems(snapshot.data!.monthlyIncome),
+                              selectedItem: _selectedMonthlyIncomeRange ?? null,
+                              callBack: (selectedValue) {
+                                setState(() {
+                                  _selectedMonthlyIncomeRange = selectedValue;
+                                });
+                              },
+                            ),
+                            SizedBox(
+                              height: 30,
+                            ),
+                            CustomDropDown(
+                              hint: 'Monthly Expense',
+                              dropdownMenuItems: buildDropDownMenuItems(snapshot.data!.monthlyExpense),
+                              selectedItem: _selectedMonthlyExpense ?? null,
+                              callBack: (selectedValue) {
+                                setState(() {
+                                  _selectedMonthlyExpense = selectedValue;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    CustomDropDown(
-                      hint: 'Monthly Income',
-                      dropdownMenuItems: buildDropDownMenuItems(snapshot.data!.monthlyIncome),
-                      selectedItem: _selectedMonthlyIncomeRange!,
-                      callBack: (selectedValue) {
-                        setState(() {
-                          _selectedMonthlyIncomeRange = selectedValue;
-                        });
-                      },
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    CustomDropDown(
-                      hint: 'Monthly Expense',
-                      dropdownMenuItems: buildDropDownMenuItems(snapshot.data!.monthlyExpense),
-                      selectedItem: _selectedMonthlyExpense!,
-                      callBack: (selectedValue) {
-                        setState(() {
-                          _selectedMonthlyExpense = selectedValue;
-                        });
-                      },
-                    ),
-                  ],
+                  ),
                 ),
-              );
-            } else {
-              return Text('Error while loading data');
-            }
-          },
-        ),
-      ),
+                Align(
+                  alignment: FractionalOffset.bottomCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: NextButton(onClick: () {
+                      widget.subScreenCallbackListener.redirectToNextScreen(0);
+                    }),
+                  ),
+                ),
+              ],
+            ),
+          );
+        } else {
+          return Text('Error while loading data');
+        }
+      },
     );
   }
-  List<DropdownMenuItem<String>> buildDropDownMenuItems(
-      List<MonthlyExpenseModel> listItems) {
+
+  List<DropdownMenuItem<String>> buildDropDownMenuItems(List<MonthlyExpenseModel> listItems) {
     List<DropdownMenuItem<String>> items = [];
     for (MonthlyExpenseModel listItem in listItems) {
       items.add(
@@ -99,16 +127,16 @@ class _PersonalInformationScreenState extends State<PersonalInformationScreen> {
     return items;
   }
 
-  bool isValidFields(){
-    if(_selectedMonthlyExpense!=null && _selectedGoalItem!=null && _selectedMonthlyIncomeRange!=null){
-      return _selectedMonthlyExpense!.isNotEmpty && _selectedGoalItem!.isNotEmpty &&  _selectedMonthlyIncomeRange!.isNotEmpty;
-    }else
+  bool isValidFields() {
+    if (_selectedMonthlyExpense != null && _selectedGoalItem != null && _selectedMonthlyIncomeRange != null) {
+      return _selectedMonthlyExpense!.isNotEmpty && _selectedGoalItem!.isNotEmpty && _selectedMonthlyIncomeRange!.isNotEmpty;
+    } else
       return false;
   }
 
   Future<DataModel> getDropDownDataModel() async {
     String string = await rootBundle.loadString('assets/data.json');
-    Map<String , dynamic > values = json.decode(string);
+    Map<String, dynamic> values = json.decode(string);
     _model = DataModel.fromJson(values);
     return _model!;
   }
